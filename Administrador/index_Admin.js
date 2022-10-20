e591be0bb87f51a3e63beac27007357aca292ad2
JuanFelipe21 <juanfelipebahamon.c@gmail.com>
2022-10-20 17:59:00 -0500

    Arreglos generales

--- Administrador/index_Admin.js
+++ Administrador/index_Admin.js
@@ -18,7 +18,31 @@ var usuarios = [user1, user2];
 var usuarioAValidar = [];
 
 function login() {
-    window.location = "../Administrador/Inicio_Admin";
+    var user, pass;
+
+    user = document.getElementById("correo").value;
+    pass = document.getElementById("contraseña").value;
+
+    usuarioAValidar = buscarElUsuarioRegistrado(user);
+
+
+
+    if (usuarioAValidar[0]) {
+        if (usuarioAValidar[0].contraseña == pass) {
+            sessionStorage.setItem('usuario', usuarioAValidar[0].correo);
+            sessionStorage.setItem('nombre', usuarioAValidar[0].nombre);
+
+            console.log(usuarioAValidar);
+            window.location = "Inicio_Admin.html";
+        } else {
+            window.alert("Contraseña incorrecta");
+        }
+
+    } else {
+        window.alert("Usuario no registrado");
+    }
+
+    sessionStorage.setItem('usuario', usuarioAValidar[0].correo);
 }
 function buscarElUsuarioRegistrado(user) {
     return usuarios.filter(usuario => usuario.correo == user)
--- Residente/index_Res.js
+++ Residente/index_Res.js
@@ -33,7 +33,7 @@ function login() {
             sessionStorage.setItem('nombre', usuarioAValidar[0].nombre);
 
             console.log(usuarioAValidar);
-            window.location = "../Residente/Inicio_Res.html";
+            window.location = "Inicio_Res.html";
         } else {
             window.alert("Contraseña incorrecta");
         }
