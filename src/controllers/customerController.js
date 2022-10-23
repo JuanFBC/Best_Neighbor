b6c53ce5dc163bffd5acb432b181f83e909fdb40
naturalcandy1 <naturalcandy31@gmail.com>
2022-10-22 20:00:08 -0500

    Base de datos

---
+++ src/controllers/customerController.js
@@ -0,0 +1,58 @@
+const controller = {};
+
+controller.list = (req, res) => {
+  req.getConnection((err, conn) => {
+    conn.query('SELECT * FROM residente', (err, residentes) => {
+     if (err) {
+      res.json(err);
+     }
+     res.render('residentes', {
+        data: residentes
+     });
+    });
+  });
+};
+
+controller.save = (req, res) => {
+  const data = req.body;
+  console.log(req.body)
+  req.getConnection((err, connection) => {
+    const query = connection.query('INSERT INTO residente set ?', data, (err, residente) => {
+      console.log(residente)
+      res.redirect('/');
+    })
+  })
+};
+
+controller.edit = (req, res) => {
+  const { id } = req.params;
+  req.getConnection((err, conn) => {
+    conn.query("SELECT * FROM residente WHERE id = ?", [id], (err, rows) => {
+      res.render('residentes_edit', {
+        data: rows[0]
+      })
+    });
+  });
+};
+
+controller.update = (req, res) => {
+  const { id } = req.params;
+  const newresidente = req.body;
+  req.getConnection((err, conn) => {
+
+  conn.query('UPDATE residente set ? where id = ?', [newresidente, id], (err, rows) => {
+    res.redirect('/');
+  });
+  });
+};
+
+controller.delete = (req, res) => {
+  const { id } = req.params;
+  req.getConnection((err, connection) => {
+    connection.query('DELETE FROM residente WHERE id = ?', [id], (err, rows) => {
+      res.redirect('/');
+    });
+  });
+}
+
+module.exports = controller;
